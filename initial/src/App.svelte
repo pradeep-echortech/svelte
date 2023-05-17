dfb8a29574ef41960535f8bf8e240e840680a799
pradeep-echortech <pradeep.p@echortech.com>
2023-05-17 10:25:25 +0530

    initial

---
+++ initial/src/App.svelte
@@ -0,0 +1,122 @@
+<script>
+	// export let name;
+	// let bold = "<b>PRADEEP</b>"
+	// const status = 'danger'
+	// const isPromoted = true
+	// let count = 0
+	// const num = 5
+	// let fullnames = [
+	// 	{first:'meher',last:'pradeep'},
+	// 	{first:'durga',last:'prasad'},
+	// 	{first:'ravi',last:'teja'},
+	// ]
+
+	// let handleClick = function(stepsize=1){
+	// 	count +=  stepsize
+	// }
+
+
+	let formValues = {
+		name : '',
+		profileSummary : '',
+		country : '',
+		skillSet : [],
+		gender:[]
+	}
+</script>
+
+<main>
+	<!-- {#if num === 0 }
+	<h1>the number is zero</h1>
+	{:else if num < 0}
+	<h1>the number is negative</h1>
+	{:else if num > 0}
+	<h1>the number is positive</h1>
+	{:else}
+	<h1>It is not a number</h1>
+	{/if}
+	<div>{@html bold}</div>
+	<h2 class={status}>status</h2>
+	<h2 class={isPromoted? 'promoted':' '}>Movie Title</h2> -->
+
+	<!-- {#each fullnames as name,index}
+		<h2>{index + 1} {name.first} {name.last}</h2>
+	{/each} -->
+
+	<!-- <button on:click={()=>(count+=1)}>Count {count}</button> -->
+	<!-- <button on:click={()=>handleClick(5)}>Count {count}</button> -->
+	<div>
+		<pre>
+			{JSON.stringify(formValues,null,2)}
+		</pre>
+	</div>
+	<form action="">
+		<div>
+			<label for="name">Name:</label>
+			<input type="text" id="name" bind:value = {formValues.name}>
+		</div>
+		<div>
+			<label for="profileSummary">Profile Summary:</label>
+			<textarea id="profileSummary" bind:value={formValues.profileSummary}></textarea>
+		</div>
+		<div>
+			<label for="country">country:</label>
+			<select id="country" bind:value={formValues.country}>
+				<option value="">Select a country</option>
+				<option value="india">india</option>
+				<option value="australia">australia</option>
+				<option value="america">america</option>
+			</select>
+		</div>
+		<div>
+			<label for="">Skill-Set:</label>
+			<input type="checkbox" id="html" value="html" bind:group={formValues.skillSet}>
+			<label for="html">HTML</label>
+			<input type="checkbox" id="css" value="css" bind:group={formValues.skillSet}>
+			<label for="html">CSS</label>
+			<input type="checkbox" id="javascript" value="javascript" bind:group={formValues.skillSet}>
+			<label for="html">JavaScript</label>
+		</div>
+		<div>
+			<label for="">Gender:</label>
+			<input type="radio" name="gender" id="male" bind:group={formValues.gender}>
+			<label for="male">Male</label>
+			<input type="radio" name="gender" id="female" bind:group={formValues.gender}>
+			<label for="female">Female</label>
+		</div>
+	</form>
+</main>
+
+<style>
+	input + label {
+		display: inline-flex;
+	}
+	.status{
+		color: green;
+	}
+	.danger{
+		color: red;
+	}
+	.promoted{
+		font-style: italic;
+	}
+	main {
+		/* text-align: center; */
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
